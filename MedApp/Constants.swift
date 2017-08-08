6e48a318d3aa43fce6cecc0057101e7d4a4f9fa1
Philip Starner <phil@twigtale.com>
2017-08-07 23:19:19 -0700

    Initial commit.  AFNetworking and AlamoFire may not be needed as the project is probably all local.

---
+++ MedApp/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  MedApp
+//
+//  Created by Philip Starner on 8/7/17.
+//  Copyright © 2017 MedApp. All rights reserved.
+//
+import Foundation
+import UIKit
+
+
+class Constants: NSObject {
+    //constant colors used in app
+    static let COLOR_MAIN = UIColor(red: 181/255.0, green: 16/255.0, blue: 58/255.0, alpha: 1.0)
+}
---
+++ Podfile
@@ -0,0 +1,11 @@
+platform :ios, '10.0'
+
+
+target 'MedApp' do 
+
+use_frameworks!
+pod 'Alamofire'
+pod 'SwiftyJSON'
+pod 'AFNetworking'
+
+end
